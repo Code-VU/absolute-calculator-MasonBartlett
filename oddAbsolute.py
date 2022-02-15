fc47bb86316432b2ffe1fa23465223d6f4fdb8cc
Mason Bartlett <Mason.Bartlett@vu.com>
2022-02-14 21:04:39 -0600

    forgot to #

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -22,5 +22,5 @@ def calculateAbsolute():
 ## uncomment calculateAbsolute() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-calculateAbsolute()
+#calculateAbsolute()
 
